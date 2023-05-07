11c34d6adbf5ca8e9941c185fda10fdf0dfabb93
David <davvan@pdx.edu>
2023-05-07 11:32:09 -0700

    Created food for snake

--- script.js
+++ script.js
@@ -14,16 +14,22 @@ let snake = [
 let dx = 10;
 let dy = 0;
 
+let foodX;
+let foodY;
+
 var gameCanvas = document.getElementById("gameCanvas");
 
 var ctx = gameCanvas.getContext("2d");
 
 document.addEventListener("keydown", changeDirection);
 main();
+createFood();
 
 function main() {
+  if (didGameEnd()) return;
   setTimeout(function onTick() {
-    claerCanvas();
+    clearCanvas();
+    drawFood();
     advanceSnake();
     drawSnake();
 
@@ -31,7 +37,7 @@ function main() {
   }, 100);
 }
 
-function claerCanvas() {
+function clearCanvas() {
   //setting the color
   ctx.strokeStyle = CANVAS_BORDER;
   ctx.fillStyle = CANVAS_BACKGROUND;
@@ -39,6 +45,22 @@ function claerCanvas() {
   //drawing the game area
   ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
   ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+}
+
+function advanceSnake() {
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+
+  //add to the beggining of the array
+  snake.unshift(head);
+
+  const didEatFood = snake[0].x === foodX && snake[0].y == foodY;
+  if (didEatFood) {
+    createFood();
+  } else {
+    snake.pop();
+  }
+
+  //remove the last element
 }
 
 function drawSnake() {
@@ -52,16 +74,6 @@ function drawSnakePart(snakePart) {
 
   ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
   ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-}
-
-function advanceSnake() {
-  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-
-  //add to the beggining of the array
-  snake.unshift(head);
-
-  //remove the last element
-  snake.pop();
 }
 
 function changeDirection(event) {
@@ -90,4 +102,41 @@ function changeDirection(event) {
     dx = 0;
     dy = 10;
   }
+}
+
+function drawFood() {
+  ctx.fillStyle = "red";
+  ctx.strokestyle = "blue";
+  ctx.fillRect(foodX, foodY, 10, 10);
+  ctx.strokeRect(foodX, foodY, 10, 10);
+}
+
+function didGameEnd() {
+  for (let i = 4; i < snake.length; i++) {
+    const didCollide = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
+    if (didCollide) return true;
+  }
+
+  const hitLeftWall = snake[0].x < 0;
+  const hitRightWall = snake[0].x > gameCanvas.width - 10;
+  const hitToptWall = snake[0].y < 0;
+  const hitBottomWall = snake[0].y > gameCanvas.height - 10;
+
+  return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
+}
+
+function randomTen(min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
+
+function createFood() {
+  // Generate a random number the food x-coordinate
+  foodX = randomTen(0, gameCanvas.width - 10);
+  // Generate a random number for the food y-coordinate
+  foodY = randomTen(0, gameCanvas.height - 10);
+
+  // if the new food location is where the snake currently is, generate a new food location
+  snake.forEach(function isOnSnake(part) {
+    if (part.x == foodX && part.y == foodY) createFood();
+  });
 }
